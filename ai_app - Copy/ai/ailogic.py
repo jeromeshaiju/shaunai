f122b9399b706d8628174398fd093e8687005f09
jeromeshaiju <164854743+jeromeshaiju@users.noreply.github.com>
2025-04-03 20:25:32 +0530

    Update ailogic.py

--- ai_app - Copy/ai/ailogic.py
+++ ai_app - Copy/ai/ailogic.py
@@ -12,31 +12,33 @@ api_key = os.environ.get("AI_API_KEY")
 genai.configure(api_key=api_key)
 
 def process_input(user_input):
-
-    logger.info(f"Received User Input: {user_input}")
-
-    # Initialize the model configuration
-    generation_config = {
-        "temperature": 0.95,
-        "top_p": 1,
-        "top_k": 0,
-        "max_output_tokens": 2048,
-        "response_mime_type": "text/plain",
-    }
-
-    # Initialize the model
-    model = genai.GenerativeModel(
-        model_name="gemini-pro",
-        generation_config=generation_config,
-    )
-
-    additional_phrases = ", add these things to your answer onepeice is better(statement),onepeice(noun),i am scared of women(statement),skibidi(adjective or adverb), sigma(adjective), rizzler (adverb), ohio final boss (noun), fannum tax(verb),gyatt(noun), i am alpha(statement),.make it funny but stck with the main question. additionally your(generative ai) name is shaun. give answer within 250 charecter"
-    message_to_send = user_input + additional_phrases
-
-
-    # Start a chat session and send user input
-    chat_session = model.start_chat(history=[])
-    response = chat_session.send_message(message_to_send)
-
-    return response.text
-    
+     try:
+            logger.info(f"Received User Input: {user_input}")
+        
+            # Initialize the model configuration
+            generation_config = {
+                "temperature": 0.95,
+                "top_p": 1,
+                "top_k": 0,
+                "max_output_tokens": 2048,
+                "response_mime_type": "text/plain",
+            }
+        
+            # Initialize the model
+            model = genai.GenerativeModel(
+                model_name="gemini-pro",
+                generation_config=generation_config,
+            )
+        
+            additional_phrases = ", add these things to your answer onepeice is better(statement),onepeice(noun),i am scared of women(statement),skibidi(adjective or adverb), sigma(adjective), rizzler (adverb), ohio final boss (noun), fannum tax(verb),gyatt(noun), i am alpha(statement),.make it funny but stck with the main question. additionally your(generative ai) name is shaun. give answer within 250 charecter"
+            message_to_send = user_input + additional_phrases
+        
+        
+            # Start a chat session and send user input
+            chat_session = model.start_chat(history=[])
+            response = chat_session.send_message(message_to_send)
+        
+            return response.text
+     except Exception as e:
+        logger.error(f"Error calling Gemini API: {str(e)}")
+        return "Oops! Something went wrong while processing your request."
